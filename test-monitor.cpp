e58684eef07e3ab73f1e002a1b89196f33948cf6
Swetha-Pradeep <swetha.pradeep@philips.com>
2024-10-01 14:52:26 +0530

    added test case to to test-monitor

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -5,6 +5,10 @@
 TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
   ASSERT_TRUE(checkTemprature(99));
   ASSERT_FALSE(checkTemprature(94.1));
+  ASSERT_FALSE(checkPulserate(102));
+  ASSERT_TRUE(checkPulserate(70));
+  ASSERT_FALSE(checkSPO2(70));
+  ASSERT_TRUE(checkSPO2(98));
   ASSERT_FALSE(vitalsOk(99, 102, 70));
   ASSERT_TRUE(vitalsOk(98.1, 70, 98));
 }
