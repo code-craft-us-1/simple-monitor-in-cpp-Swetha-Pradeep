4c5eeae1a56a956b29bf915b666b50dd228a16f1
Swetha-Pradeep <swetha.pradeep@philips.com>
2024-10-28 15:59:15 +0530

    added code to extend to warning

--- monitor.cpp
+++ monitor.cpp
@@ -14,43 +14,139 @@ void sleep() {
     }
 }
 
-bool checkThreshold(int lowThresh, int highThresh, float value) {
-    if ((value > highThresh) || (value < lowThresh)) {
-        return true;
-    }
-    return false;
-}
-int checkTemprature(int temperature) {
-    if (checkThreshold(95, 102, temperature)) {
-        cout<< "Temperature is critical!\n";
-        sleep();
-        return 0;
-    }
-   /* if (temperature > 102 || temperature < 95) {
-        cout << "Temperature is critical!\n";
-        sleep();
-        return 0;
-    }*/
-    return 1;
+void initializeTempratureLimits() {
+    tempratureLimits.push_back(std::make_pair(std::numeric_limits<int>::min(), 95));
+    tempratureMessages.push_back("Warning:HYPO_THERMIA");
+    tempratureLimits.push_back(std::make_pair(95, 96.53));
+    tempratureMessages.push_back("Warning:NEAR_HYPO");
+    tempratureLimits.push_back(std::make_pair(96.54, 100.47));
+    tempratureMessages.push_back("Normal Temprature");
+    tempratureLimits.push_back(std::make_pair(100.48, 102));
+    tempratureMessages.push_back("WARNING:NEAR_HYPER");
+    tempratureLimits.push_back(std::make_pair(102, std::numeric_limits<int>::max()));
+    tempratureMessages.push_back("HYPER_THERMIA");
 }
 
-int checkPulserate(float pulseRate) {
-    if (checkThreshold(60, 100, pulseRate)) {
-        cout << "Pulse Rate is out of range!\n";
-        sleep();
-        return 0;
-    }
-    return 1;
+void initializePulseRateLimits() {
+    pulseRateLimits.push_back(std::make_pair(std::numeric_limits<float>::min(), 60));
+    pulseRateMessages.push_back("WARNING:BRADYCHARDIA");
+    pulseRateLimits.push_back(std::make_pair(60, 100));
+    pulseRateMessages.push_back("NORMAL Pulse");
+    pulseRateLimits.push_back(std::make_pair(100, std::numeric_limits<float>::max()));
+    pulseRateMessages.push_back("WARNING:TACHYCHARDIA");
 }
 
-int checkSPO2(float spo2) {
-    if (spo2 < 90) {
-        cout << "Oxygen Saturation out of range!\n";
-        sleep();
-        return 0;
-    }
-    return 1;
+void initializeSPo2Limits() {
+    spo2Limits.push_back(std::make_pair(std::numeric_limits<float>::min(), 80));
+    spo2Messages.push_back("WARNING:HYPOXEMIA");
+    spo2Limits.push_back(std::make_pair(80, std::numeric_limits<float>::max()));
+    spo2Messages.push_back("Normal SPO2");
 }
+void writeTempratureMessage(std::string message) {
+   std::cout << message << "\n";
+}
+
+
+void writePulseMessage(std::string message) {
+    std::cout << message << "\n";
+    
+}
+
+
+void writeSPo2Message(std::string message) {
+    std::cout << message << "\n";
+
+}
+bool isTempraturNormal(int value){
+    int cnt=0;
+    for (auto i = tempratureLimits.begin(); i != tempratureLimits.end(); i++) {
+        if(i->first == std::numeric_limits<int>::min() && value < i->second){
+            writeTempratureMessage(tempratureMessages[0]);
+            
+        }
+        else if(i->second == std::numeric_limits<int>::max() && value > i->first){
+            writeTempratureMessage(tempratureMessages[4]);
+            
+        }
+        else if ((value >= i->first) && (value <= i->second)) {
+            writeTempratureMessage(tempratureMessages[cnt]);
+            
+        }
+        cnt++;
+        if(tempratureMessages[cnt-1].find("Normal") != std::string::npos){
+            
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+       
+
+    }
+}
+
+bool isPulseNormal(float value){
+    int cnt=0;
+    for (auto i = pulseRateLimits.begin(); i != pulseRateLimits.end(); i++) {
+        if(i->first == std::numeric_limits<float>::min() && value < i->second){
+            writePulseMessage(pulseRateMessages[0]);
+            
+        }
+        else if(i->second == std::numeric_limits<float>::max() && value > i->first){
+            writePulseMessage(pulseRateMessages[2]);
+            
+        }
+        else if ((value >= i->first) && (value <= i->second)) {
+            writePulseMessage(pulseRateMessages[cnt]);
+            
+        }
+        cnt++;
+        if(pulseRateMessages[cnt-1].find("Normal") != std::string::npos){
+            
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+       
+
+    }
+}
+
+bool isSpo2Normal(int value){
+    int cnt=0;
+    for (auto i = spo2Limits.begin(); i != spo2Limits.end(); i++) {
+        if(i->first == std::numeric_limits<float>::min() && value < i->second){
+            writeSPo2Message(spo2Messages[0]);
+            
+        }
+        else if(i->second == std::numeric_limits<float>::max() && value > i->first){
+            writeSPo2Message(spo2Messages[1]);
+            
+        }
+        else if ((value >= i->first) && (value <= i->second)) {
+            writeSPo2Message(spo2Messages[cnt]);
+            
+        }
+        cnt++;
+        if(spo2Messages[cnt-1].find("Normal") != std::string::npos){
+            
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+       
+
+    }
+}
+
 int vitalsOk(float temperature, float pulseRate, float spo2) {
-    return checkTemprature(temperature) && checkPulserate(pulseRate) && checkSPO2(spo2);
+    initializeTempratureLimits();
+    initializePulseRateLimits();
+    initializeSPo2Limits();
+    return isTempraturNormal(temperature) && isPulseNormal(pulseRate) && isSpo2Normal(spo2);
 }
--- monitor.h
+++ monitor.h
@@ -1,6 +1,20 @@
+#include <list>
+#include <utility>
+#include <vector>
 #pragma once
-int checkTemprature(int temperature);
-int checkPulserate(float pulseRate);
-int checkSPO2(float spo2);
+std::list<std::pair<int,int>> tempratureLimits;
+std::vector<std::string> tempratureMessages;
+std::list<std::pair<float,float>> pulseRateLimits;
+std::vector<std::string> pulseRateMessages;
+std::list<std::pair<float,float>> spo2Limits;
+std::vector<std::string> spo2Messages;
+void initializeTempratureLimits();
+
+void initializePulseRateLimits();
+void initializeSPO2Limits();
+bool isTempraturNormal(int value);
+bool isPulseNormal(float value);
+bool isSPO2Normal(int value);
+
 int vitalsOk(float temperature, float pulseRate, float spo2);
 void sleep();
