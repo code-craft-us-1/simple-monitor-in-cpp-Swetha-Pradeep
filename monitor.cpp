ae30bfe8f36188ebd7ee9c4f23166377cfbc7b55
Swetha-Pradeep <swetha.pradeep@philips.com>
2024-11-19 14:33:25 +0530

    changes made changetemprature

--- monitor.cpp
+++ monitor.cpp
@@ -3,25 +3,16 @@
 #include <thread>
 #include <chrono>
 #include <iostream>
-#include <utility>
-#include <vector>
+#include <map>
 #include <string>
-#include <limits>
-#include <list>
-#include <functional>
-#include <unordered_map>
-using std::cout , std::flush , std::this_thread::sleep_for , std::chrono::seconds;
-std::list<std::pair<int , int>> tempratureLimits;
-std::vector<std::string> tempratureMessages;
-std::list<std::pair<float , float>> pulseRateLimits;
-std::vector<std::string> pulseRateMessages;
-std::list<std::pair<float , float>> spo2Limits;
-std::vector<std::string> spo2Messages;
 
-enum ParameterType {
-    TEMPERATURE,
-    PULSE_RATE,
-    SPO2
+using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
+
+
+struct TemperatureRange {
+    int minTemp;
+    int maxTemp;
+    std::string message;
 };
 
 void sleep() {
@@ -32,155 +23,56 @@ void sleep() {
         sleep_for(seconds(1));
     }
 }
-
-template <typename T>
-void initializeLimits(std::list<std::pair<T, T>>& limits, std::vector<std::string>& messages, const std::vector<std::pair<T, T>>& limitPairs, const std::vector<std::string>& limitMessages) {
-    for (size_t i = 0; i < limitPairs.size(); ++i) {
-        limits.push_back(limitPairs[i]);
-        messages.push_back(limitMessages[i]);
+/*int checkTemprature(int temperature) {
+    if (temperature > 102 || temperature < 95) {
+        cout << "Temperature is critical!\n";
+        sleep();
+        return 0;
     }
+    return 1;
 }
+*/
 
-
-void initializeTempratureLimits() {
-    std::vector<std::pair<int, int>> tempLimits = {
-        {std::numeric_limits<int>::min(), 95},
-        {95, 96.53},
-        {96.54, 100.47},
-        {100.48, 102},
-        {102, std::numeric_limits<int>::max()}
-    };
-    std::vector<std::string> tempMessages = {
-        "Warning:HYPO_THERMIA",
-        "Warning:NEAR_HYPO",
-        "Normal Temprature",
-        "WARNING:NEAR_HYPER",
-        "HYPER_THERMIA"
-    };
-    initializeLimits(tempratureLimits, tempratureMessages, tempLimits, tempMessages);
-}
-
-void initializePulseRateLimits() {
-    std::vector<std::pair<float, float>> pulseLimits = {
-        {std::numeric_limits<float>::min(), 60},
-        {60, 100},
-        {100, std::numeric_limits<float>::max()}
-    };
-    std::vector<std::string> pulseMessages = {
-        "WARNING:BRADYCHARDIA",
-        "NORMAL Pulse",
-        "WARNING:TACHYCHARDIA"
-    };
-    initializeLimits(pulseRateLimits, pulseRateMessages, pulseLimits, pulseMessages);
-}
-
-void initializeSPo2Limits() {
-    std::vector<std::pair<float, float>> spo2ParamLimits = {
-        {std::numeric_limits<float>::min(), 80},
-        {80, std::numeric_limits<float>::max()}
-    };
-    std::vector<std::string> spo2Messages = {
-        "WARNING:HYPOXEMIA",
-        "Normal SPO2"
-    };
-    initializeLimits(spo2Limits, spo2Messages, spo2ParamLimits, spo2Messages);
-}
-
-void writeMessage(std::string message) {
-    std::cout << message << "\n";
-}
-template <typename T>
-bool isMinLimit(T value, const std::pair<T,T>& limit, const std::string& message) {
-    if (limit.first == std::numeric_limits<T>::min() && value < limit.second) {
-        writeMessage(message);
-        return true;
-    }
-    return false;
-}
-template <typename T>
-bool isMaxLimit(T value, const std::pair<T,T>& limit, const std::string& message) {
-    if (limit.second == std::numeric_limits<T>::max() && value > limit.first) {
-        writeMessage(message);
-        return true;
-    }
-    return false;
-}
-
-template<typename T>
-bool isWithinLimit(T value, const std::pair<T,T>& limit, const std::string& message) {
-    if (value >= limit.first && value <= limit.second) {
-        writeMessage(message);
-        return true;
-    }
-    return false;
-}
-
-bool isParameterNormal(double value, ParameterType type) {
-    static const std::unordered_map<ParameterType, std::function<bool(double)>> checkFunctions = {
-        { TEMPERATURE, [](double v) { return isTempraturNormal(static_cast<int>(v)); } },
-        { PULSE_RATE, [](double v) { return isPulseNormal(static_cast<float>(v)); } },
-        { SPO2, [](double v) { return isSPO2Normal(static_cast<float>(v)); } }
+int checkTemprature(int temperature) {
+    std::map<int, TemperatureRange> tempRanges = {
+        {1, {102, INT_MAX, "Temperature is critically high!\n"}},
+        {2, {100.48, 102, ""}}, // Normal range
+        {3, {96.54, 100.47, "Temperature is high!\n"}},
+        {4, {95, 96.53, "Temperature is critically high!\n"}},
+        
+        {5, {INT_MIN, 95, "Temperature is critically low!\n"}}
     };
 
-    auto it = checkFunctions.find(type);
-    if (it != checkFunctions.end()) {
-        return it->second(value);
-    }
-    return false;
-}
-
-
-bool isTempraturNormal(int value) {
-    int cnt = 0;
-    for (const auto& limit : tempratureLimits) {
-        if (isMinLimit(value, limit, tempratureMessages[0])){
-            break;
-        } 
-        else if(isMaxLimit(value, limit, tempratureMessages[4])){
-            break;
+    for (const auto& range : tempRanges) {
+        if (temperature >= range.second.minTemp && temperature <= range.second.maxTemp) {
+            if (!range.second.message.empty()) {
+                cout << range.second.message;
+                sleep();
+                return 0;
+            }
+            return 1;
         }
-        else if(isWithinLimit(value, limit, tempratureMessages[cnt])) {
-            break;
-        } 
-        cnt++;
     }
-
-    return tempratureMessages[cnt].find("Normal") != std::string::npos;
+    return 1;
 }
 
-
-bool isPulseNormal(float value) {
-    int cnt = 0;
-   for (const auto& limit : pulseRateLimits) {
-        if (isMinLimit(value, limit, pulseRateMessages[0])){
-            break;
-        } 
-        else if(isMaxLimit(value, limit, pulseRateMessages[4])){
-            break;
-        }
-        else if(isWithinLimit(value, limit, pulseRateMessages[cnt])){
-            break;
-        }
-        cnt++;
+int checkPulserate(float pulseRate) {
+    if (pulseRate < 60 || pulseRate > 100) {
+        cout << "Pulse Rate is out of range!\n";
+        sleep();
+        return 0;
     }
-    return pulseRateMessages[cnt].find("Normal") != std::string::npos;
-   
+    return 1;
 }
 
-bool isSPO2Normal(float value) {
-    int cnt = 0;
-    for (const auto& limit : spo2Limits) {
-        if (isMinLimit(value, limit, spo2Messages[0])){
-            break;
-        } 
-        else if(isMaxLimit(value, limit, spo2Messages[2])){
-            break;
-        }
-        cnt++;
+int checkSPO2(float spo2) {
+    if (spo2 < 90) {
+        cout << "Oxygen Saturation out of range!\n";
+        sleep();
+        return 0;
     }
-    return spo2Messages[cnt].find("Normal") != std::string::npos;
+    return 1;
 }
-
 int vitalsOk(float temperature, float pulseRate, float spo2) {
-    return isParameterNormal(temperature,ParameterType::TEMPERATURE) && isParameterNormal(pulseRate,ParameterType::PULSE_RATE) && isParameterNormal(spo2,ParameterType::SPO2);
+    return checkTemprature(temperature) && checkPulserate(pulseRate) && checkSPO2(spo2);
 }
--- monitor.h
+++ monitor.h
@@ -1,21 +1,6 @@
 #pragma once
-#include <list>
-#include <utility>
-#include <vector>
-#include <string>
-extern std::list<std::pair<int , int>> tempratureLimits;
-extern std::vector<std::string> tempratureMessages;
-extern std::list<std::pair<float , float>> pulseRateLimits;
-extern std::vector<std::string> pulseRateMessages;
-extern std::list<std::pair<float , float>> spo2Limits;
-extern std::vector<std::string> spo2Messages;
-void initializeTempratureLimits();
-
-void initializePulseRateLimits();
-void initializeSPo2Limits();
-bool isTempraturNormal(int value);
-bool isPulseNormal(float value);
-bool isSPO2Normal(float value);
-
+int checkTemprature(int temperature);
+int checkPulserate(float pulseRate);
+int checkSPO2(float spo2);
 int vitalsOk(float temperature, float pulseRate, float spo2);
 void sleep();
